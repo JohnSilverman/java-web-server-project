74439bb0f4f88a4e788f8c319cc4e39822a33f9e
cocobol <coco.bol@kakaocorp.com>
2022-09-15 13:17:06 +0900

    인터페이스에 새 메서드 getContentType을 추가했습니다.

--- src/main/java/http/response/ResponseBody.java
+++ src/main/java/http/response/ResponseBody.java
@@ -1,8 +1,12 @@
 package http.response;
 
+import http.MIME;
+
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public interface ResponseBody {
     public InputStream getInputStream();
     public long getContentLength();
+    public MIME getContentType();
 }
