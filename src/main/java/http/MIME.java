925d2214a393feccd407be95980c05c4c43896ac
cocobol <coco.bol@kakaocorp.com>
2022-09-15 13:12:53 +0900

    MIME 타입을 다루기 위한 enum을 추가했습니다.

--- src/main/java/http/MIME.java
+++ src/main/java/http/MIME.java
@@ -1,4 +1,41 @@
 package http;
 
 public enum MIME {
+    PLAIN_TEXT(".txt","text/plain"),
+    HTML(".html","text/html"),
+    CSS(".css","text/css"),
+    ICON(".ico", "image/x-icon"),
+    JSON(".json","application/json"),
+    GIF(".gif", "image/gif"),
+    JPEG(".jpeg","image/jpeg"),
+    JPG(".jpg","image/jpg"),
+    PNG(".png", "image/png"),
+    OCTET_STREAM("","application/octet-stream");
+
+    private String extension;
+    private String mimeType;
+
+    MIME(String extension, String mimeType){
+        this.extension = extension;
+        this.mimeType = mimeType;
+    }
+
+    public static MIME fromExtension(String extension){
+        for(var mime: MIME.values()){
+            if(mime.extension.equals(extension)) return mime;
+        }
+        return null;
+    }
+
+    public static MIME fromMime(String mimeType){
+        for(var mime: MIME.values()){
+            if(mime.mimeType.equals(mimeType)) return mime;
+        }
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return this.mimeType;
+    }
 }
