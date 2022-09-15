6b7b87c44e77ee2a5184bb3b8f01c4ae4566d8b7
cocobol <coco.bol@kakaocorp.com>
2022-09-15 14:36:09 +0900

    라우팅 함수 변경 (pathVariable 처리 가능하게) 404 응답  추가

--- src/main/java/controller/SimpleRouter.java
+++ src/main/java/controller/SimpleRouter.java
@@ -5,6 +5,7 @@ import http.response.SimpleHttpResponse;
 import http.response.HttpResponse;
 import http.response.ResponseHeader;
 import http.response.responsebody.PlainTextResponseBody;
+import util.HttpStringUtil;
 
 import java.util.Map;
 
@@ -13,21 +14,34 @@ public class SimpleRouter implements Router {
     public Map<String, Controller> controllerMap;
 
     @Override
-    public void addController(String path, Controller controller) {
-        controllerMap.put(path, controller);
+    public void addController(String pathPrefix, Controller controller) {
+        controllerMap.put(pathPrefix, controller);
     }
 
     @Override
     public HttpResponse route(HttpRequest request) {
-        HttpResponse response = new SimpleHttpResponse();
-        if(!controllerMap.containsKey(request.getPath())){
-            response.status(404)
-                    .addHeader(new ResponseHeader("Content-Type","text/plain"))
-                    .body(new PlainTextResponseBody("404 Not Found"));
-            return response;
+        // 요청 path의 prefix에 매치되는 컨트롤러 찾기
+        Controller controller = null;
+        Map<String,String> pathVars = null;
+
+        for(String pathPrefix : this.controllerMap.keySet()){
+            pathVars = HttpStringUtil.matchPath(request.getPath(), pathPrefix);
+            if(pathVars != null) {
+                controller = this.controllerMap.get(pathPrefix);
+                request.getParamMap().putAll(pathVars);
+                break;
+            }
         }
 
-        Controller controller = controllerMap.get(request.getPath());
-        return controller.getResponse();
+        return controller == null ? response404() : controller.getResponse(request);
+    }
+
+
+    private HttpResponse response404(){
+        HttpResponse response = new SimpleHttpResponse();
+        response.status(404)
+                .addHeader(new ResponseHeader("Content-Type","text/plain"))
+                .body(new PlainTextResponseBody("404 Not Found"));
+        return response;
     }
 }
