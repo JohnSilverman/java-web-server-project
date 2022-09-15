cae9e980aca6c7d66e1eb76ccbe7fe7c0c9702a2
cocobol <coco.bol@kakaocorp.com>
2022-09-16 03:00:59 +0900

    JsonResponseBody 추가

--- src/main/java/http/response/responsebody/JsonResponseBody.java
+++ src/main/java/http/response/responsebody/JsonResponseBody.java
@@ -2,22 +2,32 @@ package http.response.responsebody;
 
 import http.MIME;
 import http.response.ResponseBody;
+import util.JsonConverter;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Map;
 
 public class JsonResponseBody implements ResponseBody {
+
+    private final String jsonString;
+
+    public JsonResponseBody(Map<String,String> map){
+        jsonString = JsonConverter.mapToJsonString(map);
+    }
+
     @Override
     public InputStream getInputStream() {
-        return null;
+        return new ByteArrayInputStream(this.jsonString.getBytes());
     }
 
     @Override
     public long getContentLength() {
-        return 0;
+        return this.jsonString.length();
     }
 
     @Override
     public MIME getContentType() {
-        return null;
+        return MIME.JSON;
     }
 }
